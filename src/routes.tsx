7a3f4c70eabde55e3f2e7cbc4da9561870aefca2
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-04-12 17:04:15 +0100

    creates order listing page

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,12 +6,16 @@ import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Orders } from './pages/orders/orders'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders/> }
+    ],
   },
   {
     path: '/sign-in',
@@ -23,4 +27,5 @@ export const router = createBrowserRouter([
     element: <AuthLayout />,
     children: [{ path: '/sign-up', element: <SignUp /> }],
   },
+ 
 ])
