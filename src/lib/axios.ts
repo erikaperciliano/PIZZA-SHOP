7a391544202f2415ab087fe535479acf6d82a8b9
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-04-17 10:13:32 +0100

    add delay before the requests to test the Loading

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,3 +5,12 @@ export const api = axios.create({
     baseURL: env.VITE_API_URL,
     withCredentials: true,
 })
+
+//add delay of 2 second before the requests
+if(env.VITE_ENABLE_API_DELAY){
+    api.interceptors.request.use(async(config) => {
+        await new Promise(resolve => setTimeout(resolve, 200))
+
+        return config
+    })
+}
