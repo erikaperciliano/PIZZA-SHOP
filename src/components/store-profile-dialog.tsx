7b6549dc154016a918da0306c98c287037bc4c66
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-05-02 12:06:01 +0100

    add optimistic interface

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -15,7 +15,7 @@ import { toast } from 'sonner'
 
 const storeProfileSchema = z.object({
     name: z.string().min(1),
-    description: z.string()
+    description: z.string().nullable()
 })
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
@@ -40,21 +40,34 @@ export function StoreProfileDialog() {
             description: managedRestaurant?.description ?? ''
         }
     })
+    
+    function updateManagedRestaurantCache({ name, description }: StoreProfileSchema){
+        const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
 
+        if(cached){
+            queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
+                ...cached,
+                name,
+                description
+            })
+        }
+
+        return { cached }
+    }
 
     const  { mutateAsync: updateProfileFn } = useMutation({
         mutationFn: updateProfile,
-        onSuccess(_, { description, name }){
-            const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
+        onMutate({ name, description }) {
+           const { cached } =  updateManagedRestaurantCache({ name, description })
 
-            if(cached){
-                queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
-                    ...cached,
-                    name,
-                    description
-                })
+           return { previousProfile: cached }
+        },
+        onError(_, __, context){
+            if(context?.previousProfile){
+                updateManagedRestaurantCache(context.previousProfile)
             }
         }
+       
     })
 
     async function handleUpdateProfile(data: StoreProfileSchema){
@@ -96,7 +109,9 @@ export function StoreProfileDialog() {
                     <DialogClose asChild>
                         <Button variant='ghost' type='button'>Cancel</Button>
                     </DialogClose>
-                    <Button type='submit' variant='success' disabled={isSubmitting}>Save</Button>
+                    <DialogClose asChild>
+                        <Button type='submit' variant='success' disabled={isSubmitting}>Save</Button>
+                    </DialogClose>
                 </DialogFooter>
             </form>
         </DialogContent>
