7cdbdf227335ca4f8d4ab7ce5e03cc25cc0c6eb9
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-05-21 11:46:22 +0100

    add logout automatically

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,33 @@
 import { Header } from '@/components/header'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { api } from '@/lib/axios'
+import { isAxiosError } from 'axios'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if(isAxiosError(error)){
+          const status = error.response?.status
+          const code = error.response?.data.code
+          
+          if(status === 401 && code === 'UNAUTHORIZED'){
+            navigate('/sign-in', { replace: true })
+          }
+        }
+      }
+    )
+
+    //clean the events listener
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className='flex min-h-screen flex-col antialiased'>
       <Header/>
